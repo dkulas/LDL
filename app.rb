9c07c5596b4df3c98217dea09b528bb98dd2a11c
Danny Kulas <danny.kulas1@gmail.com>
2014-12-15 13:24:23 -0500

    commented out require relative paths

--- app.rb
+++ app.rb
@@ -4,8 +4,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
 
-require_relative #models path 
-require_relative #models path
+# require_relative models path 
+# require_relative models path
 
 enable :sessions
 
