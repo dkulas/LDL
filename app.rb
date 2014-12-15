4588fd0f5210cf5750155457fe06ae4141d2bd22
Danny Kulas <danny.kulas1@gmail.com>
2014-12-15 14:43:13 -0500

    changes being made to user model

--- app.rb
+++ app.rb
@@ -3,68 +3,68 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require_relative './models/group' 
 require_relative './models/plan'
-require_relative './model/user'
+require_relative './models/user'
 
-enable :sessions
+# enable :sessions
 
-helpers do 
-	def current_user
-		@current_user || nil
-	end
+# helpers do 
+# 	def current_user
+# 		@current_user || nil
+# 	end
 
-	def current_user?
-		@current_user == nil ? false : true
-	end
-end
+# 	def current_user?
+# 		@current_user == nil ? false : true
+# 	end
+# end
 
-before do 
-	#something to do with session
-	@current_user = User.find_by(:id => session[:user_id])
-end
+# before do 
+# 	#something to do with session
+# 	@current_user = User.find_by(:id => session[:user_id])
+# end
 
 get '/' do
 	erb :index
 end
 
-get '/users' do 
-	if current_user?
-		erb :profile
-	else 
-		redirect('/session/login')
-	end
-end
+# get '/users' do 
+# 	if current_user?
+# 		erb :profile
+# 	else 
+# 		redirect('/session/login')
+# 	end
+# end
 
-get '/session/login' do 
-	erb :login
-end
+# get '/session/login' do 
+# 	erb :login
+# end
 
-post '/session/login' do
-	user = User.find_by(:email => params[:email])
-	if user && user.authenticate(params[:password])
-		session[:user_id] = user.id
-		redirect('/users')
-	else
-		"Invalid email or password. Try again."
-		erb :login
-	end
-end
+# post '/session/login' do
+# 	user = User.find_by(:email => params[:email])
+# 	if user && user.authenticate(params[:password])
+# 		session[:user_id] = user.id
+# 		redirect('/users')
+# 	else
+# 		"Invalid email or password. Try again."
+# 		erb :login
+# 	end
+# end
 
-get '/session/sign_up' do 
-	erb :sign_up
-end
+# get '/session/sign_up' do 
+# 	erb :sign_up
+# end
 
-post '/session/sign_up' do 
-	user = User.new(params)
-	if user.save
-		session[:user_id] = user.id
-		redirect('/users')
-	else
-		@user = user_id
-		erb :sign_up
-	end
-end
+# post '/session/sign_up' do 
+# 	user = User.new(params)
+# 	if user.save
+# 		session[:user_id] = user.id
+# 		redirect('/users')
+# 	else
+# 		@user = user_id
+# 		erb :sign_up
+# 	end
+# end
 
-get '/session/logout' do 
-	session.clear
-	redirect('/')
-end
+# get '/session/logout' do 
+# 	session.clear
+# 	redirect('/')
+# end
--- models/user.rb
+++ models/user.rb
@@ -1,38 +1,4 @@
 require "bcrypt"
-
 class User < ActiveRecord::Base
-  # attributes
-  attr_accessor :password
-
-  # callbacks
-  before_save :encrypt_password
-
-  # relationships
-  has_many :plans
-  belongs_to_many :groups
-
-  # validations
-  validates_confirmation_of :password
-  validates_presence_of :password, :on => :create
-  validates :email,
-    :presence => true,
-    :uniqueness => { case_sensitive: false },
-    :length => { maximum: 255 },
-    :format => { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-
-  # methods
-  def authenticate(password)
-    if BCrypt::Password.new(self.password) == password
-      return self
-    else
-      return nil
-    end
-  end
-
-  def encrypt_password
-    if password.present?
-      return self.password = BCrypt::Password.create(password)
-    end
-  end
-
+  
 end
