4a8a6a5da5190d3f319eb29b82908d68d1f600e1
Danny Kulas <danny.kulas1@gmail.com>
2014-12-16 09:54:56 -0500

    updated user model

--- models/user.rb
+++ models/user.rb
@@ -1,17 +1,12 @@
-require "bcrypt"
+require 'bcrypt'
 
 class User < ActiveRecord::Base
 	has_many :plans
 	has_and_belongs_to_many :groups
-	
-  include BCrypt
 
-  def password
-    @password ||= Password.new(password_hash)
-  end
-
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
+  def encrypt_password
+    if password.present?
+      return self.password_digest = BCrypt::Password.create(password)
+    end
   end
 end
