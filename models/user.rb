3c8180e71c80084edc9debfc5a637b6a550830e6
Danny Kulas <danny.kulas1@gmail.com>
2014-12-16 12:19:28 -0500

    Updated user model

--- models/user.rb
+++ models/user.rb
@@ -1,16 +1,44 @@
-require 'bcrypt'
-require_relative '../db/connection'
+require "bcrypt"
+require_relative "../db/connection"
 
 class User < ActiveRecord::Base
-	before_save :encrypt_password
-	attr_accessor :password
+  # attributes
+  attr_accessor :password
 
-	has_many :plans
-	has_and_belongs_to_many :groups
+  # callbacks
+  before_save :encrypt_password
+
+  # relationships
+  has_and_belongs_to_many :groups
+  has_many :plans
+
+  # validations
+  validates_confirmation_of :password
+  validates_presence_of :password, :on => :create
+  validates :name,
+    :presence => true,
+    :length => { maximum: 255 }
+  validates :email,
+    :presence => true,
+    :uniqueness => { case_sensitive: false },
+    :length => { maximum: 255 },
+    :format => { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+   validates :phone_number,
+   	:presence => true
+
+  # methods
+  def authenticate(password)
+    if BCrypt::Password.new(self.password_digest) == password
+      return self
+    else
+      return nil
+    end
+  end
 
   def encrypt_password
     if password.present?
       return self.password_digest = BCrypt::Password.create(password)
     end
   end
+
 end
