ec61e0b762d6eff3a99dff93b6e9ac22901a1668
Danny Kulas <danny.kulas1@gmail.com>
2014-12-16 12:19:04 -0500

    updated sign up and login routes

--- app.rb
+++ app.rb
@@ -1,5 +1,3 @@
-#LDL APP
-require 'BCrypt'
 require 'sinatra'
 require 'sinatra/activerecord'
 require_relative './models/group' 
@@ -8,66 +6,65 @@ require_relative './models/user'
 
 enable :sessions
 
-helpers do 
-	def current_user
-		@current_user || nil
-	end
+helpers do
+  def current_user
+    @current_user || nil
+  end
 
-	def current_user?
-		@current_user == nil ? false : true
-	end
+  def current_user?
+    @current_user == nil ? false : true
+  end
 end
 
-before do 
-	@errors ||= []
-	@current_user = User.find_by(:id => session[:user_id])
+before do
+  #session[:cart] ||= [] NEED TO ASSOCIATE THIS WITH USER PLANS
+  @errors ||= []
+  @current_user = User.find_by(:id => session[:user_id])
 end
 
-get '/' do
-	erb :index
+get "/" do
+  erb :index
 end
 
-get '/users' do 
-	if current_user?
-		erb :profile
-	else 
-		redirect('/session/login')
-	end
+get "/users" do
+  if current_user?
+    erb :profile
+  else
+    redirect('/session/login')
+  end
 end
 
-get '/session/login' do 
-	erb :login
+get '/session/login' do
+  erb :login
 end
 
-post '/session/login' do
-	user = User.find_by(:email => params[:email])
-	if user && user.authenticate(params[:password])
-		session[:user_id] = user.id
-		redirect('/users')
-	else
-		@errors << "Invalid email or password. Try again!"
-		erb :login
-	end
+post "/session/login" do
+  user = User.find_by(:email => params[:email])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect('/users')
+  else
+    @errors << "Invalid email or password. Try again!"
+    erb :login
+  end
 end
 
-get '/session/sign_up' do 
-	erb :sign_up
+get '/session/sign_up' do
+  erb :sign_up
 end
 
-# POTENTIAL SOLUTION FOR SIGN UP
 post "/session/sign_up" do
-	@email = params["email"]
-	@password = params["password"]
-	@password_digest = BCrypt::Password.create(@password)
-	
-	
-	user = User.create
-	
-	session[:user_id] = user.id
-	redirect("/users")
+  user = User.new(params)
+  if user.save
+    session[:user_id] = user.id
+    redirect('/users')
+  else
+    @user = user
+    erb :sign_up
+  end
 end
 
-get '/session/logout' do 
-	session.clear
-	redirect('/')
+get "/session/logout" do
+  session.clear
+  redirect('/')
 end
