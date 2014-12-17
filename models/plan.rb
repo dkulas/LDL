7e267e1e41aa529b709c42bd2c461fc37e87459f
Danny Kulas <danny.kulas1@gmail.com>
2014-12-17 09:56:25 -0500

    updated plan validations

--- models/plan.rb
+++ models/plan.rb
@@ -2,7 +2,7 @@ class Plan < ActiveRecord::Base
 	belongs_to :user
 	
 
-	validates :user_id, :presence => true
+	validates :name, :presence => true
 	validates :location, :presence => true
 	validates :time, :presence => true
 end
